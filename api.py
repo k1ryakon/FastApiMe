ad3ac6886ddb3f053d0834547c2e4accc3432108
SAVELO <papush367@gmail.com>
2025-03-12 17:23:44 +0300

    123

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Body
+from uvicorn import uvicorn
 
 app = FastAPI()
 
@@ -7,30 +8,35 @@ messages_db = {0: "First post in FastAPI"}
 
 
 @app.get("/")
-async def get_all_messages() -> str:
-    return 'asdf'
+async def get_all_messages() -> dict:
+    return messages_db
 
 
 @app.get("/message/{message_id}")
 async def get_message(message_id: int) -> str:
-    pass
+    return messages_db[message_id]
 
-
-@app.post("/message")
-async def create_message(message: str) -> str:
-    pass
-
+@app.post("/message", status_code=status.HTTP_201_CREATED)
+async def create_message(pipka: str = Body()) -> str:
+    current_index = int(max(messages_db, key=int)) + 1
+    messages_db[current_index] = pipka
+    return "Message created!"
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: int, message: str) -> str:
-    pass
-
+async def update_message(message_id: int, message: str = Body()) -> str:
+    messages_db[message_id] = message
+    return "Message updated!"
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: int) -> str:
-    pass
-
+    messages_db.pop(message_id)
+    return f"Message ID={message_id} deleted!"
 
 @app.delete("/")
 async def kill_message_all() -> str:
-    pass
+    messages_db.clear()
+    return "All messages deleted!"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='localhost')
